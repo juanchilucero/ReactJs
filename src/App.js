6c509b2012b950293f43abd9d4d5b3c8343250ad
Juan Ignacio Lucero Vazquez <juanchi98lucero@gmail.com>
2023-12-30 12:02:24 -0300

    agregando nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import NavBar from './components/NavBar'; // Importa el componente NavBar
+import ItemListContainer from './components/ItemListContainer'; // Importa el componente ItemListContainer
 
 function App() {
   return (
     <div className="App">
+      <NavBar /> {/* Renderiza el componente NavBar */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <ItemListContainer greeting="¡Hola! Bienvenido a la tienda." /> {/* Renderiza el componente ItemListContainer */}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,15 +3,49 @@ import CartWidget from './CartWidget';
 
 const NavBar = () => {
   return (
-    <nav>
-      <div className="nav-wrapper">
-        <a href="#!" className="brand-logo">Ecommerce</a>
-        <ul className="right hide-on-med-and-down">
-          <li><a href="#!">Home</a></li>
-          <li><a href="#!">Products</a></li>
-          {/* ... Otros elementos del menú */}
-          <li><CartWidget /></li>
-        </ul>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#!">Mi Tienda</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#!">Inicio</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#!"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Categorías
+              </a>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a className="dropdown-item" href="#!">Categoría 1</a></li>
+                <li><a className="dropdown-item" href="#!">Categoría 2</a></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><a className="dropdown-item" href="#!">Ver todas las categorías</a></li>
+              </ul>
+            </li>
+            {/* ... Otros elementos del menú */}
+            <li className="nav-item">
+              <CartWidget />
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
