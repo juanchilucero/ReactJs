b6748a104f2e444f8da3f93f8231e84d0dbf4e7e
Juan Ignacio Lucero Vazquez <juanchi98lucero@gmail.com>
2024-02-03 11:45:15 -0300

    pre2

--- src/App.js
+++ src/App.js
@@ -9,20 +9,6 @@ function App() {
   return (
     <div className="App">
       <NavBar /> {/* Renderiza el componente NavBar */}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <ItemListContainer greeting="¡Hola! Bienvenido a la tienda." /> {/* Renderiza el componente ItemListContainer */}
     </div>
   );
